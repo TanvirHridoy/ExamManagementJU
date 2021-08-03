903baa650bc652502fc1e7b7cb9494d942e3f843
Administrator <Administrator@HRIDOYPCWORK>
2021-08-04 00:19:23 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CertificationMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
